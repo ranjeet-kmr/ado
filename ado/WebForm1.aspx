b0e3ba44deba08f7332d3b0128957247c50a1ba8
Ranjeet Kumar <v-ranjku@microsoft.com>
2020-04-15 22:52:22 +0530

    Add project files.

---
+++ ado/WebForm1.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ado.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridView1" runat="server">
+            </asp:GridView>
+            <br />
+            <asp:GridView ID="GridView2" runat="server">
+            </asp:GridView>
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ado/WebForm1.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace ado
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // var con = new SqlConnection("data source=RANJEET\\NOOB; database=Ranjeet; integrated security =SSPI");
+            // var cmd = new SqlCommand("select * from sales", con);
+            // con.Open();
+            //// SqlDataReader rdr = cmd.ExecuteReader();
+            ////GridView1.DataSource = rdr;
+            // GridView1.DataSource = cmd.ExecuteReader();
+            // GridView1.DataBind();
+            // con.Close();
+
+            //string con = "data source=RANJEET\\NOOB; database=Ranjeet; integrated security =SSPI";
+            //using(var sqlcon = new SqlConnection(con))
+            //{
+            //    var sqlcmd = new SqlCommand("select * from sales", sqlcon);
+            //    sqlcon.Open();
+            //    GridView1.DataSource = sqlcmd.ExecuteReader();
+            //    GridView1.DataBind();
+            //}
+
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (var con = new SqlConnection(cs))
+            {
+                var cmd = new SqlCommand("select * from tblProduct; select * from tblProductSales", con);
+                con.Open();
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    GridView1.DataSource = dr;
+                    GridView1.DataBind();
+
+                    while (dr.NextResult())
+                    {
+                        GridView2.DataSource = dr;
+                        GridView2.DataBind();
+                    }
+
+                }
+
+            }
+        }
+    }
+}
